eb22145c62da7e104c4499a1a200d4770dba0f10
Cipto Hartanto <cipto.hartanto@yahoo.com>
2022-01-22 22:54:30 +0800

    ✨ enabling formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,42 @@
 module.exports = {
   siteMetadata: {
     title: `Yayasan Flores Penuh Kasih`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yayasanflorespenuhkasih.com`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "VYjKLlqc5fNJg2c4tAcNoZjQEcVmrXz8jJBpqqV9okc",
-      "spaceId": "r5986177nqt8"
-    }
-  }, "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "123"
-    }
-  }, "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/img/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/img/"
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "VYjKLlqc5fNJg2c4tAcNoZjQEcVmrXz8jJBpqqV9okc",
+        spaceId: "r5986177nqt8",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "123",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/img/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/img/",
+      },
+      __key: "images",
+    },
+  ],
 };
