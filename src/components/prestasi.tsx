b4396478002829569347199bc8454f042928b75d
Kurma Ramadhan <kurma-ramadhan@outlook.com>
2022-06-07 09:41:25 +0800

    💄 update layout styling of <Prestasi />

--- src/components/prestasi.tsx
+++ src/components/prestasi.tsx
@@ -6,12 +6,33 @@ const Aktitivas = ({ content }) => {
   return (
     <div className="prestasi">
       <h3 className="typography typography-title2">{judulPrestasi}</h3>
+
+      <div className="prestasi-video">
+        {kontenPrestasi.map((item) => {
+          return (
+            <>
+              {item.file.contentType === "video/mp4" && (
+                <div
+                  className="gridImage-item gridImage-item--large"
+                  key={item.file.url}
+                >
+                  <video className="rekomendasi-video" controls>
+                    <source src={item.file.url + "#t=1"} type="video/mp4" />
+                  </video>
+
+                  <h3 className="rekomendasi-title">{item.description}</h3>
+                </div>
+              )}
+            </>
+          );
+        })}
+      </div>
       <div className="prestasi-grid">
         <div className="gridImage gridImage--layout2">
           {kontenPrestasi.map((item) => {
             return (
               <>
-                {item.file.contentType === "image/jpeg" ? (
+                {item.file.contentType === "image/jpeg" && (
                   <div
                     key={item.file.url}
                     className="gridImage-item"
@@ -19,17 +40,6 @@ const Aktitivas = ({ content }) => {
                       backgroundImage: `url("https:${item.file.url}")`,
                     }}
                   />
-                ) : (
-                  <div
-                    className="gridImage-item gridImage-item--large"
-                    key={item.file.url}
-                  >
-                    <video className="rekomendasi-video" controls>
-                      <source src={item.file.url + "#t=1"} type="video/mp4" />
-                    </video>
-
-                    <h3 className="rekomendasi-title">{item.description}</h3>
-                  </div>
                 )}
               </>
             );
