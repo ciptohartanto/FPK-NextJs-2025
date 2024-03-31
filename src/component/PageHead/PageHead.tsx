41bfca198e0e5f08db502f986703111c191cc5ea
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-03-31 22:48:00 +0800

    ✨ Next 14 Boilerplate - Page Router, Stylelint, Husky, EsLint

---
+++ src/component/PageHead/PageHead.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function PageHead() {
+  return (
+    <Head>
+      <title>Boilerplate</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta name="description" content="description" />
+      <meta name="keywords" content="keywords" />
+    </Head>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import PageHead from '@/component/PageHead'
+
+export default function HomePage() {
+  return (
+    <>
+      <PageHead />
+      <div>
+        <h1>Boilerplate</h1>
+      </div>
+    </>
+  )
+}
