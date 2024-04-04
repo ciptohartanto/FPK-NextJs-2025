3d0aea437a2cbc05d2ac9d8a84b1d3fabd7dc464
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-05 02:51:45 +0800

    💄 add font Urbanist and all necessary font related settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/index.scss'
 
 import type { AppProps } from 'next/app'
+import { Urbanist } from 'next/font/google'
 
+const urbanist = Urbanist({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+})
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={urbanist.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
