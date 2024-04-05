c15455d4706119a507915522c0dac206a38abac6
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-05 14:04:17 +0800

    🧱 add svgr config to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      cleanupIds: false,
+                      removeViewBox: false,
+                    },
+                  },
+                },
+                'removeXMLNS',
+              ],
+            },
+          },
+        },
+      ],
+    })
+
+    return config
+  },
+}
 
 export default nextConfig
