651f68931af467fa556073d02758514867cc9ffe
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-05 20:29:00 +0800

    🎨 create <Trademark /> and add styling

---
+++ src/elements/Trademark.tsx
@@ -0,0 +1,8 @@
+export default function Trademark() {
+  return (
+    <div className="trademark">
+      <span className="trademark-line1">Cipto</span>
+      <span className="trademark-line2">Hartanto</span>
+    </div>
+  )
+}
