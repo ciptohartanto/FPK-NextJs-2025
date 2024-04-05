66e229460a75a1f65467291d1349f12385a2da3e
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-06 00:42:16 +0800

    🧱 add <Layout />, update project structure and add styling

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames'
+import { Urbanist } from 'next/font/google'
+import { ReactNode } from 'react'
+
+const urbanist = Urbanist({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+  variable: '--font-urbanist',
+})
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className={classNames('layout', urbanist.className)}>{children}</div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,13 @@
 import '../styles/index.scss'
 
 import type { AppProps } from 'next/app'
-import { Urbanist } from 'next/font/google'
 
-const urbanist = Urbanist({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '700'],
-  style: ['normal', 'italic'],
-  display: 'swap',
-})
+import Layout from '@/layout/Layout'
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={urbanist.className}>
+    <Layout>
       <Component {...pageProps} />
-    </main>
+    </Layout>
   )
 }
