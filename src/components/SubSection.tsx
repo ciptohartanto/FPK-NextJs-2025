7aa161ba83a96f7318a4f078b6662b097e3e5061
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-06 22:21:10 +0800

    💄 create <SubSection /> and add styling

---
+++ src/components/SubSection.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+interface SubSectionProps {
+  title: string
+  children: ReactNode
+}
+
+export default function SubSection({ title, children }: SubSectionProps) {
+  return (
+    <div className="subSection">
+      <h3 className="subSection-title">{title}</h3>
+      <div className="subSection-row">{children}</div>
+    </div>
+  )
+}
