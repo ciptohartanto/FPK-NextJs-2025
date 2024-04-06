30b4eaf7e39e7dff1eec174e2c790d3ecc92fc9d
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-06 15:49:14 +0800

    🐛 refine styling of <Hero />

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,16 +5,18 @@ import Trademark from '@/elements/Trademark'
 export default function Hero() {
   return (
     <section className="hero">
-      <div className="hero-wrapper">
-        <Trademark />
-        <h2 className="hero-subtitle">Senior Web Developer</h2>
-        <h3 className="hero-caption">
-          8+ years of problem solving, strong communication skills, and making
-          websites
-        </h3>
-        <Link className="hero-link" href="/resume">
-          Read More
-        </Link>
+      <div className="hero-row">
+        <div className="hero-colWrapper">
+          <Trademark />
+          <h2 className="hero-subtitle">Senior Web Developer</h2>
+          <h3 className="hero-caption">
+            8+ years of problem solving, strong communication skills, and making
+            websites
+          </h3>
+          <Link className="hero-link" href="/resume">
+            Read More
+          </Link>
+        </div>
       </div>
     </section>
   )
