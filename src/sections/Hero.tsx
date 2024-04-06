a05c16e01ac7aece0627feaaeb9f6d54c78a58c7
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-06 14:18:08 +0800

    🎨 refine <Hero /> to include grid system

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,15 +5,17 @@ import Trademark from '@/elements/Trademark'
 export default function Hero() {
   return (
     <section className="hero">
-      <Trademark />
-      <h2 className="hero-subtitle">Senior Web Developer</h2>
-      <h3 className="hero-caption">
-        8+ years of problem solving, strong communication skills, and making
-        websites
-      </h3>
-      <Link className="hero-link" href="/resume">
-        Read More
-      </Link>
+      <div className="hero-wrapper">
+        <Trademark />
+        <h2 className="hero-subtitle">Senior Web Developer</h2>
+        <h3 className="hero-caption">
+          8+ years of problem solving, strong communication skills, and making
+          websites
+        </h3>
+        <Link className="hero-link" href="/resume">
+          Read More
+        </Link>
+      </div>
     </section>
   )
 }
