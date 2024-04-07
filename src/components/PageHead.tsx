1cf4ab93326f9e4bf9876b1d8f7e148911c46f3a
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-07 13:04:43 +0800

    🏷️ update title of <PageHead /> and name of package.json

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function PageHead() {
   return (
     <Head>
-      <title>Boilerplate</title>
+      <title>Cipto Hartanto</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <meta name="description" content="description" />
