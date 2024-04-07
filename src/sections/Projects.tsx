e78e49356e5154b29df2ca8d69d21183209edfd8
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-07 17:20:19 +0800

    ✨ create <Projects /> with working <Swiper />

---
+++ src/sections/Projects.tsx
@@ -0,0 +1,114 @@
+// Import Swiper styles
+import 'swiper/css'
+
+import classNames from 'classnames'
+import { useState } from 'react'
+import { Autoplay } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import Tag from '@/elements/Tag'
+
+const DATA = [
+  {
+    title: 'Finschia Network (v2)',
+    date: 'Released on Jan 01, 2024',
+    backgroundUrl:
+      'https://cdn.dribbble.com/userupload/2773310/file/original-8805cbb2d31a4ffd237af14003091ad3.jpg',
+    tags: [
+      { text: 'Next.js' },
+      { text: 'Framer Motion (React)' },
+      { text: 'CMS' },
+    ],
+  },
+  {
+    title: 'LINE SPOT',
+    date: '2020 - 2022',
+    backgroundUrl:
+      'https://cdn.dribbble.com/userupload/11909609/file/original-9a8a27073f1e775e9f948a5348b98ade.png',
+    tags: [
+      { text: 'Next.js' },
+      { text: 'TypeScript' },
+      { text: 'GraphQL' },
+      { text: 'StyledComponent' },
+    ],
+  },
+  {
+    title: 'LINE Fact Checker',
+    date: 'mid 2019 - late 2019',
+    backgroundUrl:
+      'https://cdn.dribbble.com/users/6627804/screenshots/17128773/media/8519df95636a1e5f6bcf157ba2fe544c.png',
+    tags: [{ text: 'Vue.js (v2)' }, { text: 'Pug' }, { text: 'SCSS' }],
+  },
+  {
+    title: 'LINE TODAY',
+    date: '2017 - 2019',
+    backgroundUrl:
+      'https://cdn.dribbble.com/userupload/12097710/file/original-0002e31a1d04d0a9574ae7c517b9b821.png',
+    tags: [{ text: 'HTML' }, { text: 'SCSS' }],
+  },
+]
+
+export default function Projects() {
+  const [currentSlideId, setCurrentSlideId] = useState(0)
+  const totalSlides = DATA.length
+  return (
+    <section className="projects">
+      <div className="projects-wrapper">
+        <h3 className="projects-title">Projects</h3>
+        <div className="projects-swiperWrapper">
+          <Swiper
+            wrapperTag="ul"
+            slidesPerView={1}
+            spaceBetween={30}
+            breakpoints={{ 850: { slidesPerView: 1.7, spaceBetween: 32 } }}
+            className="projects-swiperModule"
+            autoplay
+            loop
+            modules={[Autoplay]}
+            onSlideChange={(swiper) => {
+              setCurrentSlideId(swiper.realIndex + 1)
+            }}
+          >
+            {DATA.map((item) => (
+              <SwiperSlide
+                key={item.title}
+                tag="li"
+                className="projects-swiperSlide"
+              >
+                {({ isActive }) => (
+                  <div
+                    className={classNames('projects-swiperContentWrapper', {
+                      'projects-swiperContentWrapper--active': isActive,
+                    })}
+                  >
+                    <div
+                      className="projects-swiperThumbnail"
+                      style={{ backgroundImage: `url(${item.backgroundUrl})` }}
+                    />
+                    <div className="projects-swiperTextWrapper">
+                      <h3 className="projects-swiperTitle">{item.title}</h3>
+                      <span className="projects-swiperDate">{item.date}</span>
+                      <ul className="projects-swiperTags">
+                        {item.tags.map((tag) => (
+                          <li className="projects-swiperTag" key={tag.text}>
+                            <Tag text={tag.text} />
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                )}
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+        <div className="projects-counter">
+          <span className="projects-counterText">
+            {currentSlideId}/{totalSlides}
+          </span>
+        </div>
+      </div>
+    </section>
+  )
+}
