c903c155a6ed94460e98c547cb284ac82e6383dd
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-08 12:27:48 +0800

    💫 add entrance animation to <Nav />

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -31,88 +31,110 @@ export default function Nav() {
   const [isFolded, setIsFolded] = useState(true)
 
   return (
-    <div className="nav">
-      <button onClick={() => setIsFolded(!isFolded)} className="nav-icon">
-        {/* display IconHamburger when isFolded */}
-        <span className="nav-iconWrapper">
-          <IconHamburger isActive={isFolded} type={IconHamburgerTypes.LARGE} />
-        </span>
+    <AnimatePresence>
+      <motion.div
+        className="nav"
+        initial="hide"
+        animate="animate"
+        exit="hide"
+        transition={{ type: 'tween' }}
+        variants={{
+          hide: {
+            opacity: 0,
+            x: 20,
+          },
+          animate: {
+            opacity: 1,
+            x: 0,
+            transition: { delay: 2, duration: 0.2 },
+          },
+        }}
+      >
+        <button onClick={() => setIsFolded(!isFolded)} className="nav-icon">
+          {/* display IconHamburger when isFolded */}
+          <span className="nav-iconWrapper">
+            <IconHamburger
+              isActive={isFolded}
+              type={IconHamburgerTypes.LARGE}
+            />
+          </span>
 
-        {/* display IconHamburger when !isFolded */}
-        <span className="nav-iconWrapper">
-          <IconX isActive={!isFolded} type={IconXTypes.LARGE} />
-        </span>
-      </button>
-      <AnimatePresence>
-        {!isFolded && (
-          <motion.div
-            className="nav-wrapper"
-            initial="hide"
-            animate="animate"
-            exit="hide"
-            transition={{ type: 'tween' }}
-            variants={{
-              hide: {
-                opacity: 0,
-                x: '100%',
-              },
-              animate: {
-                opacity: 1,
-                x: 0,
-              },
-            }}
-          >
-            <motion.nav
-              className="nav-navigation"
+          {/* display IconHamburger when !isFolded */}
+          <span className="nav-iconWrapper">
+            <IconX isActive={!isFolded} type={IconXTypes.LARGE} />
+          </span>
+        </button>
+        <AnimatePresence>
+          {!isFolded && (
+            <motion.div
+              className="nav-wrapper"
               initial="hide"
               animate="animate"
-              transition={{ type: 'tween', delay: 0.7, duration: 0.6 }}
+              exit="hide"
+              transition={{ type: 'tween' }}
               variants={{
                 hide: {
-                  filter: 'drop-shadow(0px 0 40px rgba(0,0,0,0))',
+                  opacity: 0,
+                  x: '100%',
                 },
                 animate: {
-                  filter: 'drop-shadow(-35px 0 40px rgba(0,0,0,0.05))',
+                  opacity: 1,
+                  x: 0,
                 },
               }}
             >
-              <ul className="nav-list">
-                {NAV_LINKS.map((item, idx) => (
-                  <motion.li
-                    key={item.text}
-                    className="nav-item"
-                    initial="hide"
-                    animate="animate"
-                    transition={{ type: 'tween' }}
-                    whileHover={{
-                      x: 10,
-                      transition: { delay: 0, duration: 0.2 },
-                    }}
-                    variants={{
-                      hide: {
-                        opacity: 0,
-                        filter: 'blur(2px)',
-                        y: -10,
-                      },
-                      animate: {
-                        opacity: 1,
-                        filter: 'blur(0px)',
-                        y: 0,
-                        transition: {
-                          delay: 0.285 * (idx + 1),
-                          duration: 0.3,
+              <motion.nav
+                className="nav-navigation"
+                initial="hide"
+                animate="animate"
+                transition={{ type: 'tween', delay: 0.7, duration: 0.6 }}
+                variants={{
+                  hide: {
+                    filter: 'drop-shadow(0px 0 40px rgba(0,0,0,0))',
+                  },
+                  animate: {
+                    filter: 'drop-shadow(-35px 0 40px rgba(0,0,0,0.05))',
+                  },
+                }}
+              >
+                <ul className="nav-list">
+                  {NAV_LINKS.map((item, idx) => (
+                    <motion.li
+                      key={item.text}
+                      className="nav-item"
+                      initial="hide"
+                      animate="animate"
+                      transition={{ type: 'tween' }}
+                      whileHover={{
+                        x: 10,
+                        transition: { delay: 0, duration: 0.2 },
+                      }}
+                      variants={{
+                        hide: {
+                          opacity: 0,
+                          filter: 'blur(2px)',
+                          y: -10,
                         },
-                      },
-                    }}
-                  >
-                    <span className="nav-text">{item.text}</span>
-                  </motion.li>
-                ))}
-              </ul>
-            </motion.nav>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
+                        animate: {
+                          opacity: 1,
+                          filter: 'blur(0px)',
+                          y: 0,
+                          transition: {
+                            delay: 0.285 * (idx + 1),
+                            duration: 0.3,
+                          },
+                        },
+                      }}
+                    >
+                      <span className="nav-text">{item.text}</span>
+                    </motion.li>
+                  ))}
+                </ul>
+              </motion.nav>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </AnimatePresence>
   )
 }
