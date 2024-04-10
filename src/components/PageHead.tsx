cf2c7344c7d7e03574138ed0b75864bcd0a0ab0e
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-10 18:48:36 +0800

    ✨ (っ◕‿◕)っ

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -1,13 +1,33 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 interface PageHeadProps {
   pageTitle: string
 }
 
 export default function PageHead({ pageTitle }: PageHeadProps) {
+  const [currentTitle, setCurrentTitle] = useState('')
+
+  useEffect(() => {
+    const desiredTitle = `Cipto Hartanto | ${pageTitle}`
+    const updateCurrentTitle = () => {
+      if (document.visibilityState === 'hidden') {
+        setCurrentTitle('miss you (っ◕‿◕)っ')
+      } else {
+        setCurrentTitle(desiredTitle)
+      }
+    }
+
+    updateCurrentTitle()
+
+    document.addEventListener('visibilitychange', updateCurrentTitle)
+
+    return () =>
+      document.removeEventListener('visibilitychange', updateCurrentTitle)
+  }, [pageTitle])
   return (
     <Head>
-      <title>{'Cipto Hartanto | ' + pageTitle}</title>
+      <title>{currentTitle}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <meta name="description" content="description" />
