680f25388fe39ca1f170a52a872911b15c3891da
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-10 18:32:44 +0800

    🐛 add cssClassName prop to <SubSection />

--- src/components/SubSection.tsx
+++ src/components/SubSection.tsx
@@ -1,13 +1,19 @@
+import classNames from 'classnames'
 import { ReactNode } from 'react'
 
 interface SubSectionProps {
   title: string
+  cssClassName?: string
   children: ReactNode
 }
 
-export default function SubSection({ title, children }: SubSectionProps) {
+export default function SubSection({
+  title,
+  cssClassName,
+  children,
+}: SubSectionProps) {
   return (
-    <div className="subSection">
+    <div className={classNames('subSection', cssClassName)}>
       <h3 className="subSection-title">{title}</h3>
       <div className="subSection-row">{children}</div>
     </div>
