2d7fdea40413ae8b01e3e3814e1b0f125606f4a2
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-15 13:54:11 +0800

    💄 adjust styling to make it better

--- src/sections/ArticleBody.tsx
+++ src/sections/ArticleBody.tsx
@@ -31,76 +31,78 @@ export default function ArticleBody({
 
   return (
     <div className="articleBody">
-      <div className="articleBody-wrapperTop">
-        <h2 className="articleBody-title">{title}</h2>
-        <span>Published on {formatDate(publishTime)}</span>
-        <ul className="articleBody-wrapperTags">
-          {tags.split(',').map((text) => (
-            <li key={text} className="articleBody-tag">
-              <Tag text={text} />
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="articleBody-wrapperBottom">
-        <div className="articleBody-wrapperBottomLeft">
-          <TableOfContent />
-          <ul className="articleBody-nav">
-            <li className="articleBody-navItem  articleBody-navItem--main">
-              <AnchorLink href="../#writings">
-                <span className="articleBody-navLink">
-                  Check other writings
-                </span>
-              </AnchorLink>
-            </li>
-            {prevArticle && (
-              <li className="articleBody-navItem articleBody-navItem--prev">
-                <AnchorLink href={prevArticle?.slug}>
-                  <span className="articleBody-navLink">
-                    {prevArticle?.title}
-                  </span>
-                </AnchorLink>
+      <div className="articleBody-wrapper">
+        <div className="articleBody-wrapperTop">
+          <h2 className="articleBody-title">{title}</h2>
+          <span>Published on {formatDate(publishTime)}</span>
+          <ul className="articleBody-wrapperTags">
+            {tags.split(',').map((text) => (
+              <li key={text} className="articleBody-tag">
+                <Tag text={text} />
               </li>
-            )}
-            {nextArticle && (
-              <li className="articleBody-navItem articleBody-navItem--next">
-                <AnchorLink href={nextArticle.slug}>
-                  <span className="articleBody-navLink">
-                    {nextArticle.title}
-                  </span>
-                </AnchorLink>
-              </li>
-            )}
+            ))}
           </ul>
         </div>
-        <div
-          className={classNames(
-            'articleBody-wrapperSection',
-            'articleBody-section'
-          )}
-        >
-          <Markdown
-            components={{
-              h3(props) {
-                const { children } = props
-                return <h3 className={HEADING3_CSS}>{children}</h3>
-              },
-              code(props) {
-                const { children, className, ...rest } = props
-                const lang = className?.split('language-')[1]
-                return (
-                  <CodeSnippet
-                    content={String(children)}
-                    lang={lang}
-                    {...rest}
-                  />
-                )
-              },
-            }}
+
+        <div className="articleBody-wrapperBottom">
+          <div className="articleBody-wrapperBottomLeft">
+            <TableOfContent />
+            <ul className="articleBody-nav">
+              <li className="articleBody-navItem  articleBody-navItem--main">
+                <AnchorLink href="../#writings">
+                  <span className="articleBody-navLink">
+                    Check other writings
+                  </span>
+                </AnchorLink>
+              </li>
+              {prevArticle && (
+                <li className="articleBody-navItem articleBody-navItem--prev">
+                  <AnchorLink href={prevArticle?.slug}>
+                    <span className="articleBody-navLink">
+                      {prevArticle?.title}
+                    </span>
+                  </AnchorLink>
+                </li>
+              )}
+              {nextArticle && (
+                <li className="articleBody-navItem articleBody-navItem--next">
+                  <AnchorLink href={nextArticle.slug}>
+                    <span className="articleBody-navLink">
+                      {nextArticle.title}
+                    </span>
+                  </AnchorLink>
+                </li>
+              )}
+            </ul>
+          </div>
+          <div
+            className={classNames(
+              'articleBody-wrapperSection',
+              'articleBody-section'
+            )}
           >
-            {articleContent}
-          </Markdown>
+            <Markdown
+              components={{
+                h3(props) {
+                  const { children } = props
+                  return <h3 className={HEADING3_CSS}>{children}</h3>
+                },
+                code(props) {
+                  const { children, className, ...rest } = props
+                  const lang = className?.split('language-')[1]
+                  return (
+                    <CodeSnippet
+                      content={String(children)}
+                      lang={lang}
+                      {...rest}
+                    />
+                  )
+                },
+              }}
+            >
+              {articleContent}
+            </Markdown>
+          </div>
         </div>
       </div>
     </div>
--- src/sections/ResumeBody.tsx
+++ src/sections/ResumeBody.tsx
@@ -14,53 +14,61 @@ export default function ResumeBody({ componentData }: ResumeBodyProps) {
 
   return (
     <div className="resumeBody">
-      <div className="resumeBody-wrapperTop">
-        <Trademark />
-        <div className="resumeBody-iconPdf">
-          <span className="resumeBody-iconPdfText">PDF</span>
+      <div className="resumeBody-wrapper">
+        <div className="resumeBody-wrapperTop">
+          <Trademark />
+          <div className="resumeBody-iconPdf">
+            <span className="resumeBody-iconPdfText">PDF</span>
+          </div>
         </div>
-      </div>
-      <section className="resumeBody-wrapperSection">
-        {resumeData.map((subsection) => {
-          if (subsection.__typename === EntityTypeName.SubSection) {
-            return (
-              <SubSection
-                key={subsection.title}
-                title={subsection.title}
-                cssClassName="resumeBody-subsection"
-              >
-                <div
-                  className="resumeBody-paragraph"
-                  dangerouslySetInnerHTML={{ __html: subsection.content.html }}
-                />
-              </SubSection>
-            )
-          } else {
-            if ('experienceItem' in subsection) {
+        <section className="resumeBody-wrapperSection">
+          {resumeData.map((subsection) => {
+            if (subsection.__typename === EntityTypeName.SubSection) {
               return (
                 <SubSection
                   key={subsection.title}
                   title={subsection.title}
                   cssClassName="resumeBody-subsection"
                 >
-                  {subsection.experienceItem.map((experienceItemData, idx) => (
-                    <ExperienceItem
-                      key={idx}
-                      isLastItem={subsection.experienceItem.length === idx + 1}
-                      type={
-                        idx === 0
-                          ? ExPerienceItemTypes.CURRENT
-                          : ExPerienceItemTypes.PREVIOUS
-                      }
-                      componentData={experienceItemData}
-                    />
-                  ))}
+                  <div
+                    className="resumeBody-paragraph"
+                    dangerouslySetInnerHTML={{
+                      __html: subsection.content.html,
+                    }}
+                  />
                 </SubSection>
               )
+            } else {
+              if ('experienceItem' in subsection) {
+                return (
+                  <SubSection
+                    key={subsection.title}
+                    title={subsection.title}
+                    cssClassName="resumeBody-subsection"
+                  >
+                    {subsection.experienceItem.map(
+                      (experienceItemData, idx) => (
+                        <ExperienceItem
+                          key={idx}
+                          isLastItem={
+                            subsection.experienceItem.length === idx + 1
+                          }
+                          type={
+                            idx === 0
+                              ? ExPerienceItemTypes.CURRENT
+                              : ExPerienceItemTypes.PREVIOUS
+                          }
+                          componentData={experienceItemData}
+                        />
+                      )
+                    )}
+                  </SubSection>
+                )
+              }
             }
-          }
-        })}
-      </section>
+          })}
+        </section>
+      </div>
     </div>
   )
 }
