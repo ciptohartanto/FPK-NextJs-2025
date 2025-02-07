840db12a6a1fe6f6f8e6c702cd9aea13e925200c
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-07 18:30:15 +0800

    🔥 remove older files and unused packages

--- src/utils/formatDate.ts
+++
@@ -1,7 +0,0 @@
-export default function formatDate(date: string) {
-  const newDate = new Date(date)
-  const dateString = newDate.toDateString() // returns "Thu Dec 12 2024"
-  const dateArray = dateString.split(' ')
-  const formattedDate = `${dateArray[1]} ${dateArray[2]}, ${dateArray[3]}` // "TDec 12, 2024"
-  return formattedDate
-}
