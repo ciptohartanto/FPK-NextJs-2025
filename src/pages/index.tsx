c73c55fcf72d7e30dc7f9045365e878dee43e26a
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-16 23:37:53 +0800

    🚧 implement <ButtonHollow /> to index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import ButtonHollow from '@/components/ButtonHollow'
 import TitleWithPipe from '@/components/TitleWithPipe'
 
 export default function HomePage() {
   return (
     <>
       <TitleWithPipe titleText="Hello, World!" style="onLightBackground" />
+      <ButtonHollow
+        style="one"
+        buttonText="Button Text"
+        buttonDescription="this is description"
+      />
     </>
   )
 }
