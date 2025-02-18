21d0f0fa2204d73d1e0bc1c2d9ea38a3eb834f1e
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-18 16:06:26 +0800

    ✨ create <YouTubeVid />

---
+++ src/components/YouTubeVid.tsx
@@ -0,0 +1,47 @@
+import classNames from 'classnames'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import IconYoutube from '@/assets/icons/icon-youtube.svg'
+
+type YouTubeVidProps = {
+  videoUrl: string
+  thumbnailUrl: string
+  title: string
+}
+
+const CSS_BASE_CLASS = {
+  self: 'youtubeVid',
+  topWrapper: 'youtubeVid-topWrapper',
+  image: 'youtubeVid-image',
+  icon: 'youtubeVid-icon',
+  typography: 'typography-caption',
+  title: 'youtubeVid-title',
+}
+
+export default function YouTubeVid({
+  videoUrl,
+  title,
+  thumbnailUrl,
+}: YouTubeVidProps) {
+  return (
+    <Link className={CSS_BASE_CLASS.self} href={videoUrl}>
+      <div className={CSS_BASE_CLASS.topWrapper}>
+        <Image
+          className={CSS_BASE_CLASS.image}
+          width={560}
+          height={175}
+          src={thumbnailUrl}
+          alt={title}
+        />
+
+        <IconYoutube className={CSS_BASE_CLASS.icon} />
+      </div>
+      <h4
+        className={classNames(CSS_BASE_CLASS.title, CSS_BASE_CLASS.typography)}
+      >
+        {title}
+      </h4>
+    </Link>
+  )
+}
