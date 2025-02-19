880c7bedb3a2725414d885fa34177241ac2b5beb
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-19 13:25:35 +0800

    ✨ add Nokora and Niconne fonts to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/index.scss'
 
+import classNames from 'classnames'
 import type { AppProps } from 'next/app'
+import { Niconne, Nokora } from 'next/font/google'
+
+const nokora = Nokora({
+  variable: '--font-nokora',
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  display: 'swap',
+})
+const niconne = Niconne({
+  variable: '--font-niconne',
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+})
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={classNames(nokora.variable, niconne.variable)}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
