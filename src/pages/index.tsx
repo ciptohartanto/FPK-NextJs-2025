79ab4f85d09cee55cc5d9cecd52ed7250d812044
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-20 17:44:55 +0800

    🚧 implement <Cta /> to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Sns from '@/components/Sns'
 import Timeline from '@/components/Timeline'
 import TitleWithPipe from '@/components/TitleWithPipe'
 import YouTubeVid from '@/components/YouTubeVid'
+import Cta from '@/sections/Cta'
 import Jumbotron from '@/sections/Jumbotron'
 import Prestasi from '@/sections/Prestasi'
 import Quote from '@/sections/Quote'
@@ -62,6 +63,35 @@ Kami percaya bahwa sesuatu yang luar biasa akan terjadi ketika kami membulatkan 
       <Prestasi
         title="Prestasi"
         content="Kegigihan dan komitmen yang serius dari para mahasiswa-mahasiswi di Yayasan Flores Penuh Kasih membuahkan hasil yang tidak hanya berdampak pada diri mereka, tetapi juga di hotel tempat mereka bekerja."
+      />
+      <Cta
+        title="Bergabung Bersama Kami"
+        buttons={[
+          {
+            url: 'https://example.com',
+            buttonText: 'Sebagai Murid',
+            buttonDescription:
+              'Tantangannya sangat sulit, tetapi dengan keyakinan yang teguh kita pasti bisa.',
+
+            style: 'one',
+          },
+          {
+            url: 'https://example.com',
+            buttonText: 'Sebagai Murid',
+            buttonDescription:
+              'Tantangannya sangat sulit, tetapi dengan keyakinan yang teguh kita pasti bisa.',
+
+            style: 'two',
+          },
+          {
+            url: 'https://example.com',
+            buttonText: 'Sebagai Murid',
+            buttonDescription:
+              'Tantangannya sangat sulit, tetapi dengan keyakinan yang teguh kita pasti bisa.',
+
+            style: 'three',
+          },
+        ]}
       />
     </>
   )
