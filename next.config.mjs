02935eda5b2c452a0e223909dd691aed2cace188
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-24 15:38:47 +0800

    ✨ enable receiving image data from us-west-2.graphassets.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'us-west-2.graphassets.com',
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
