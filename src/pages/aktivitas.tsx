b822a3f5aace4bba79e74ce2cd85cc9603c75911
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-25 19:52:45 +0800

    🔥 remove unused dummy data

--- src/pages/aktivitas.tsx
+++ src/pages/aktivitas.tsx
@@ -16,31 +16,7 @@ export default function AktivitasHome({ aktivitasPage }: AktivitasPageProps) {
   return (
     <div style={{ width: '100%' }}>
       <Album type="withNumbers" imagesOnAlbum={albums} />
-      <YouTubes
-        title="Yayasan Flores Penuh Kasih on Youtube"
-        youtubeVids={[
-          {
-            videoUrl: 'https://example.com',
-            thumbnailUrl: '/dummy-wallpaper.jpg',
-            title: 'Kunjungan ke Loccal Collection',
-          },
-          {
-            videoUrl: 'https://example.com',
-            thumbnailUrl: '/dummy-wallpaper.jpg',
-            title: 'Kunjungan ke Loccal Collection',
-          },
-          {
-            videoUrl: 'https://example.com',
-            thumbnailUrl: '/dummy-wallpaper.jpg',
-            title: 'Kunjungan ke Loccal Collection',
-          },
-          {
-            videoUrl: 'https://example.com',
-            thumbnailUrl: '/dummy-wallpaper.jpg',
-            title: 'Kunjungan ke Loccal Collection',
-          },
-        ]}
-      />
+      <YouTubes title="Yayasan Flores Penuh Kasih on Youtube" />
     </div>
   )
 }
