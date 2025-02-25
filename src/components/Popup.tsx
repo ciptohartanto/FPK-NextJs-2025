9e8717bd90ec523023625ed9d2ed58f94f21d50f
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-25 11:31:07 +0800

    🐛 update popup props and add contact info

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,16 +1,19 @@
 import classNames from 'classnames'
-import { ReactElement } from 'react'
 
 import IconClose from '@/assets/icons/icon-close.svg'
 
 import ButtonHollowRounded from './ButtonHollowRounded'
 import TitleWithPipe from './TitleWithPipe'
 
-type PopupProps = {
+export type PopupDataProps = {
   popupTitle: string
   isWithButtons: boolean
   contentBodyType: '50-50' | '30-70' | '60-40' | '100-0'
-  content: ReactElement[]
+  content: string[]
+}
+
+type PopupProps = {
+  data: PopupDataProps
 }
 
 const CSS_BASE_CLASS = {
@@ -24,12 +27,13 @@ const CSS_BASE_CLASS = {
   buttonItem: 'popup-buttonItem',
 }
 
-export default function Popup({
-  popupTitle,
-  isWithButtons,
-  contentBodyType,
-  content,
-}: PopupProps) {
+const CONTACT_INFO = {
+  tel: 'tel:+6281268317567',
+  email: 'mailto:yayasanflorespenuhkasih@hotmail.com',
+}
+
+export default function Popup({ data }: PopupProps) {
+  const { popupTitle, isWithButtons, contentBodyType, content } = data
   return (
     <div className={CSS_BASE_CLASS.self}>
       <div className={CSS_BASE_CLASS.background} />
@@ -46,19 +50,22 @@ export default function Popup({
                   `${CSS_BASE_CLASS.content}--${contentBodyType}`
                 )}
                 key={index}
-              >
-                {item}
-              </div>
+                dangerouslySetInnerHTML={{ __html: item }}
+              />
             )
           })}
         </div>
         {isWithButtons && (
           <ul className={CSS_BASE_CLASS.buttonList}>
             <li className={CSS_BASE_CLASS.buttonItem}>
-              <ButtonHollowRounded buttonText="Kontak via email" />
+              <a href={CONTACT_INFO.email}>
+                <ButtonHollowRounded buttonText="Kontak via email" />
+              </a>
             </li>
             <li className={CSS_BASE_CLASS.buttonItem}>
-              <ButtonHollowRounded buttonText="Kontak via telepon" />
+              <a href={CONTACT_INFO.tel}>
+                <ButtonHollowRounded buttonText="Kontak via telepon" />
+              </a>
             </li>
           </ul>
         )}
