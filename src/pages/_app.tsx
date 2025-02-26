9ae4bfa8ac2902b5fb6024791cc943586140465d
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-02-26 18:41:35 +0800

    🐛 fix build failed issue because of undefined value of theFooter coming from <Layout />

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import Layout from '@/layout'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <FpkProvider>
-      <Layout pageProps={pageProps}>
+      <Layout theFooter={pageProps.theFooter}>
         <Component {...pageProps} />
       </Layout>
     </FpkProvider>
