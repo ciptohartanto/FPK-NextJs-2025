e44dcb895cf78d28871ed88df4d2d89a96e92d44
Cipto Hartanto <cipto.hartanto@yahoo.com>
2025-03-02 16:19:15 +0800

    ✨ include GoogleAnalytics and GoogleTagManager to _app and enable debug mode only when development

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,21 @@
 import '../styles/index.scss'
 
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import ReactGA from 'react-ga4'
 
 import { FpkProvider } from '@/context'
 import Layout from '@/layout'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID as string, {
-      gaOptions: {
-        // debug_mode: true,
-        cookie_flags: 'Secure;SameSite=None',
-        cookieDomain: 'none',
-      },
-    })
-  }, [])
-
   return (
     <FpkProvider>
       <Layout theFooter={pageProps.theFooter}>
         <Component {...pageProps} />
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM as string} />
+        <GoogleAnalytics
+          gaId={process.env.NEXT_PUBLIC_GA_ID as string}
+          debugMode={process.env.NODE_ENV === 'development'}
+        />
       </Layout>
     </FpkProvider>
   )
